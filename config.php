d464afac94d115934c63bb28c1ac9471df690e3b
dinka202 <dianalakiza64@gmail.com>
2026-05-12 18:08:57 +0300

    Основное задание

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'vk_token' => 'vk1.a.t5fUafLb8MCHQcYotvDouOcW_-5nxrr-5-wOnJjUAAsVfESUWzGnE43-CKb8KQyJOSDQCLWmeiz_sWxyulM6izLkLSeEe7ODXiGeu7IPoVTdfhuHUHKoitPPXJL7qX1BuxVeIcIPFXJ7h6w83FY15oB3hujJ5XIRK-r1N5Ki_vQYjUzaYE7ublgQoV0mZaHup77nLQh4-IKWb98fEzlbtA',
-    'vk_api_version' => '5.199', // Версия API VK
+    'vk_api_version' => '5.199',
     'mysql_host' => '127.0.0.1',
     'mysql_user' => 'root',
     'mysql_password' => 'qwerty123_qw',
